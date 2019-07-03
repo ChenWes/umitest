6514e7f249adac52867c218a1f05ff6b435c7da1
ChenWes <chenxuhua0530@163.com>
2019-07-03 08:38:06 +0800

    remove space

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 # set -e
 # set -x
-
-export NODE_ENV="${NODE_ENV:-development}"
-
+export NODE_ENV="${NODE_ENV:development}"
 echo "Docker Start To Build ======================================================start"
 echo "$NODE_ENV"
 echo "Docker Start To Build ======================================================end"
-
 if [ $NODE_ENV == "development" ]; then
   # this runs webpack-dev-server with hot reloading
   yarn start
 else
   # build the app and serve it via nginx
   yarn build
-
-
   mkdir -p $ROOT/logs/nginx
   nginx -g 'daemon off;' -c $ROOT/src/nginx.conf
   nginx -c $ROOT/src/nginx.conf
